cebf7999d98a42774d59a5c70c82c29433405e3c
Prashant Kumar <prashant63957174@gmail.com>
2026-05-09 11:33:27 +0530

    update

--- client/src/app/api/booking/[id]/arriving/route.ts
+++ client/src/app/api/booking/[id]/arriving/route.ts
@@ -2,17 +2,43 @@ import { NextRequest, NextResponse } from "next/server";
 import connectDb from "@/lib/db";
 import Booking from "@/models/booking.model";
 
+// export async function POST(
+//   req: NextRequest,
+//   { params }: { params: { id: string } }
+// ) {
+//   await connectDb();
+
+//   const booking = await Booking.findById(params.id);
+//   if (!booking)
+//     return NextResponse.json({ message: "Not found" }, { status: 404 });
+
+// booking.status = "arriving";
+
+//   await booking.save();
+
+//   return NextResponse.json({ success: true });
+// }
+
+
+
 export async function POST(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   await connectDb();
 
-  const booking = await Booking.findById(params.id);
-  if (!booking)
-    return NextResponse.json({ message: "Not found" }, { status: 404 });
+  const { id } = await params;
 
-booking.status = "arriving";
+  const booking = await Booking.findById(id);
+
+  if (!booking) {
+    return NextResponse.json(
+      { message: "Not found" },
+      { status: 404 }
+    );
+  }
+
+  booking.status = "arriving";
 
   await booking.save();
 

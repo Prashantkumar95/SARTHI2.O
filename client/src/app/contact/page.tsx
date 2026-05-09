e5fa88150c13ee71d865abc590a8d81a24517263
Prashant Kumar <prashant63957174@gmail.com>
2026-05-09 23:25:20 +0530

    udate

--- client/src/app/contact/page.tsx
+++ client/src/app/contact/page.tsx
@@ -40,7 +40,7 @@ export default function Contact() {
                   Customer Care
                 </p>
                 <p className="text-lg font-medium">
-                  +91 98765 43210
+                  +91 98755xxxx
                 </p>
               </div>
 
@@ -49,7 +49,7 @@ export default function Contact() {
                   Office Address
                 </p>
                 <p className="text-lg font-medium">
-                  Gwalior, Madhya Pradesh, India
+                  Jaypee University India
                 </p>
               </div>
 
